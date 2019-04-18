7d878599391b1829c6228e2babc9761be24cee10
Your Name <you@example.com>
2019-04-18 13:19:21 +0800

    init

---
+++ t02.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use CGI;
+my $q = CGI->new;
+print $q->header;
+
+print "Hello World!";
---
+++ t03.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use CGI::Simple;
+my $q = CGI::Simple->new;
+print $q->header;
+
+print "Hello World!";
